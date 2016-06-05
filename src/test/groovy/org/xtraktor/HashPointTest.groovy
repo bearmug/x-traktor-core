314595f1cd4258dbff07cffcaeef9d87d130981b
bearmug <pavel.fadeev@gmail.com>
2016-06-05 20:34:34 +0300

    commit test for hash precision degradation

--- src/test/groovy/org/xtraktor/HashPointTest.groovy
+++ src/test/groovy/org/xtraktor/HashPointTest.groovy
@@ -3,15 +3,21 @@ package org.xtraktor
 import com.javadocmd.simplelatlng.Geohasher
 import com.javadocmd.simplelatlng.LatLng
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class HashPointTest extends Specification {
 
-    def "check close points have similar geohash"() {
+    @Unroll
+    def "check close points have similar geohash, delta: #lonDelta / #latDelta"() {
+
+        def lon = 54.254775
+        def lat = 41.995358
+
         given:
         HashPoint p1 = new HashPoint(geoHashFull: Geohasher.hash(
-                new LatLng(lon1 as Double, lat1 as Double)))
+                new LatLng(lon as Double, lat as Double)))
         HashPoint p2 = new HashPoint(geoHashFull: Geohasher.hash(
-                new LatLng(lon2 as Double, lat2 as Double)))
+                new LatLng(lon + lonDelta, lat + latDelta)))
 
         when:
         String hash1 = p1.getGeoHashFull()
@@ -21,30 +27,34 @@ class HashPointTest extends Specification {
         hash1.take(hashPrecision as int) == hash2.take(hashPrecision as int)
 
         where:
-        lon1      | lat1      | lon2      | lat2      | hashPrecision
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        lonDelta | latDelta | hashPrecision
+        0.000001 | 0.0      | 10
+        0.0      | 0.000001 | 10
+        0.000001 | 0.000001 | 10
 
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        0.00001  | 0.0      | 7
+        0.0      | 0.00001  | 7
+        0.00001  | 0.00001  | 7
 
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        0.0001   | 0.0      | 7
+        0.0      | 0.0001   | 7
+        0.0001   | 0.0001   | 7
 
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        0.001    | 0.0      | 7
+        0.0      | 0.001    | 7
+        0.001    | 0.001    | 7
 
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        0.01     | 0.0      | 5
+        0.0      | 0.01     | 5
+        0.01     | 0.01     | 5
 
-        54.345673 | 43.239947 | 54.345674 | 43.239947 | 10
-        54.345673 | 43.239947 | 54.345673 | 43.239948 | 10
-        54.345673 | 43.239947 | 54.345674 | 43.239948 | 10
+        0.1      | 0.0      | 3
+        0.0      | 0.1      | 3
+        0.1      | 0.1      | 3
+
+        1.0      | 0.0      | 2
+        0.0      | 1.0      | 2
+        1.0      | 1.0      | 2
     }
 
     def "lon/lat precision limited with 6 digits"() {
