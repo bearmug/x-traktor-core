890ea58bbf099565ec9250a72951f48d5229972e
bearmug <pavel.fadeev@gmail.com>
2016-06-12 21:42:46 +0300

    dummy data storage implementation

---
+++ src/main/java/org/xtraktor/storage/SimpleDataStorage.java
@@ -0,0 +1,18 @@
+package org.xtraktor.storage;
+
+import org.xtraktor.DataStorage;
+import org.xtraktor.HashPoint;
+
+import java.util.stream.Stream;
+
+public class SimpleDataStorage implements DataStorage {
+    @Override
+    public boolean save(Stream<HashPoint> points) {
+        return false;
+    }
+
+    @Override
+    public Stream<HashPoint> findByHashAndTime(HashPoint input, int hashPrecision) {
+        return null;
+    }
+}
