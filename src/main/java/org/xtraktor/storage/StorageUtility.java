8e5636fff486c3544a8f579e61fb8f747db7b647
bearmug <pavel.fadeev@gmail.com>
2016-06-21 23:56:33 +0300

    introduce jackson serialization

--- src/main/java/org/xtraktor/storage/StorageUtility.java
+++ src/main/java/org/xtraktor/storage/StorageUtility.java
@@ -1,15 +1,29 @@
 package org.xtraktor.storage;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.xtraktor.HashPoint;
+
+import java.io.IOException;
 
 public class StorageUtility {
 
+    private final ObjectMapper mapper = new ObjectMapper();
+
     String serialize(HashPoint p) {
-        return null;
+        try {
+            return mapper.writeValueAsString(p);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Can not serialize: " + p);
+        }
     }
 
     HashPoint deserialize(String s) {
-        return null;
+        try {
+            return mapper.readValue(s, HashPoint.class);
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not parse: " + s);
+        }
     }
 
     String generateKey(HashPoint point, int precision) {
