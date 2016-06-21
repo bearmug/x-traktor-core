c97a8c46f0127cbba3af228393b325aaac286ba8
bearmug <pavel.fadeev@gmail.com>
2016-06-22 00:56:00 +0300

    feature redis test with minimal boilerplate

--- src/main/java/org/xtraktor/storage/StorageUtility.java
+++ src/main/java/org/xtraktor/storage/StorageUtility.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.xtraktor.HashPoint;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 
 class StorageUtility {
 
@@ -14,7 +15,7 @@ class StorageUtility {
         try {
             return mapper.writeValueAsString(p);
         } catch (JsonProcessingException e) {
-            throw new IllegalStateException("Can not serialize: " + p);
+            throw new IllegalStateException("Can not serialize: " + p, e);
         }
     }
 
@@ -22,11 +23,19 @@ class StorageUtility {
         try {
             return mapper.readValue(s, HashPoint.class);
         } catch (IOException e) {
-            throw new IllegalStateException("Can not parse: " + s);
+            throw new IllegalStateException("Can not parse: " + s, e);
         }
     }
 
     String getKey(HashPoint point, int precision) {
         return point.getHash(precision) + "-" + point.getTimestamp();
+    }
+
+    public int getFreePort() {
+        try (ServerSocket tempServer = new ServerSocket(0)) {
+            return tempServer.getLocalPort();
+        } catch (IOException e) {
+            throw new IllegalStateException("Can not allocate free port", e);
+        }
     }
 }
--- src/test/groovy/org/xtraktor/storage/BigLocalDataStorageTest.groovy
+++ src/test/groovy/org/xtraktor/storage/BigLocalDataStorageTest.groovy
@@ -1,12 +1,32 @@
 package org.xtraktor.storage
 
+import groovy.util.logging.Slf4j
+import redis.embedded.RedisServer
+import spock.lang.Shared
 import spock.lang.Specification
 
+@Slf4j
 class BigLocalDataStorageTest extends Specification {
 
+    @Shared
+    StorageUtility utility = new StorageUtility()
+    RedisServer redisServer
 
+    def setup() {
+        log.debug("Starting embedded redis server...")
+        redisServer = new RedisServer(utility.freePort)
+        redisServer.start()
+        log.info("Embedded redis server started, port: ${redisServer.port}")
+    }
 
-    def setup() {}
+    def "do nothing test"() {
+        expect:
+        1 == 1
+    }
 
-    def cleanup() {}
+    def cleanup() {
+        log.debug("Stopping embedded redis server...")
+        redisServer.stop()
+        log.info("Embedded redis server stopped, port: ${redisServer.port}")
+    }
 }
