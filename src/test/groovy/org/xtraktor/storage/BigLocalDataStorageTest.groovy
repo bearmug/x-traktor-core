98cf4a95a0cdf1b1a42033d458401191678a066d
bearmug <pavel.fadeev@gmail.com>
2016-06-22 00:59:57 +0300

    provide initial test for redis storage

--- src/test/groovy/org/xtraktor/storage/BigLocalDataStorageTest.groovy
+++ src/test/groovy/org/xtraktor/storage/BigLocalDataStorageTest.groovy
@@ -1,9 +1,12 @@
 package org.xtraktor.storage
 
 import groovy.util.logging.Slf4j
+import org.xtraktor.DataStorage
+import org.xtraktor.HashPoint
 import redis.embedded.RedisServer
 import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Unroll
 
 @Slf4j
 class BigLocalDataStorageTest extends Specification {
@@ -19,9 +22,28 @@ class BigLocalDataStorageTest extends Specification {
         log.info("Embedded redis server started, port: ${redisServer.port}")
     }
 
-    def "do nothing test"() {
-        expect:
-        1 == 1
+    static final long USER_ID = 777
+    static final long TIME = Long.MAX_VALUE
+
+    @Unroll
+    def "point saved with full timestamp and precision == #precision"() {
+        given:
+        DataStorage storage = new BigLocalDataStorage("localhost", redisServer.port);
+
+        when:
+        storage.save([point].stream())
+        HashPoint stored = storage.findByHashAndTime(input, precision).findAny().get()
+
+        then:
+        stored?.userId == USER_ID
+        stored?.timestamp == input.timestamp
+        stored?.geoHashFull == point.geoHashFull
+
+        where:
+        point                                            | input                                                | precision
+        new HashPoint('1234567890', 0, 0, TIME, USER_ID) | new HashPoint('1234567890', 0, 0, TIME, USER_ID + 1) | 8
+        new HashPoint('1234567890', 0, 0, TIME, USER_ID) | new HashPoint('123456789X', 0, 0, TIME, USER_ID + 1) | 8
+        new HashPoint('1234567890', 0, 0, TIME, USER_ID) | new HashPoint('12345678XX', 0, 0, TIME, USER_ID + 1) | 8
     }
 
     def cleanup() {
