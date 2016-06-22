8c8ae79fe95c23f638661d1958d1dc128bfca0cc
bearmug <pavel.fadeev@gmail.com>
2016-06-23 00:19:58 +0300

    javadoc for DataPreprocessor

--- src/main/java/org/xtraktor/DataPreprocessor.java
+++ src/main/java/org/xtraktor/DataPreprocessor.java
@@ -8,5 +8,27 @@ import java.util.stream.Stream;
  * output with predefined discretion time/space discretion.
  */
 public interface DataPreprocessor {
+
+    /**
+     * Consuming input {@link RawPoint} and:
+     * <ul>
+     * <li>bring input into the order</li>
+     * <li>filter invalid elements</li>
+     * <li>composing points into the sequence</li>
+     * <li>for each points pair into the sequence:
+     * <ul>
+     * <li>lookup for a number of time-aligned intermediate locations</li>
+     * <li>generate {@link HashPoint} for each time-aligned location</li>
+     * <li>such time-aligned points always have strict timestamp assigned</li>
+     * <li>each of generated {@link HashPoint} assigned with geohash
+     * with pre-defined precision</li>
+     * </ul>
+     * </li>
+     * </ul>
+     *
+     * @param input {@link RawPoint} incoming route for specific user. Pease be
+     *              sure that same userId for each element there
+     * @return stream with {@link HashPoint}s built for incoming route
+     */
     Stream<HashPoint> normalize(List<RawPoint> input);
 }
