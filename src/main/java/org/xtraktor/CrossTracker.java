48656f24cf35b170be322dac0cde6867e48ebd4b
bearmug <pavel.fadeev@gmail.com>
2016-06-23 00:51:53 +0300

    javadoc Crosstracker - main entry point for lib simpe usage

--- src/main/java/org/xtraktor/CrossTracker.java
+++ src/main/java/org/xtraktor/CrossTracker.java
@@ -3,7 +3,12 @@ package org.xtraktor;
 import org.xtraktor.location.LocationConfig;
 import org.xtraktor.mining.SimpleDataMiner;
 import org.xtraktor.preprocessing.SimpleDataPreprocessor;
+import org.xtraktor.storage.BigLocalDataStorage;
+import org.xtraktor.storage.SimpleDataStorage;
 
+/**
+ * Use this class as an entry point for library simplified use-cases
+ */
 public class CrossTracker {
 
     private final DataPreprocessor preprocessor;
@@ -14,15 +19,37 @@ public class CrossTracker {
         this.miner = new SimpleDataMiner(dataStorage);
     }
 
-    public static CrossTracker create(
-            LocationConfig config, DataStorage dataStorage) {
-        return new CrossTracker(config, dataStorage);
+    /**
+     * Create component, backed by local storage
+     *
+     * @param config configuration to obtain instance
+     * @return instance ready to work
+     */
+    public static CrossTracker withLocal(LocationConfig config) {
+        return new CrossTracker(config, new SimpleDataStorage());
     }
 
+    /**
+     * @param config configuration to obtain instance
+     * @param host   Redis host
+     * @param port   Redis port
+     * @return instance ready to work
+     */
+    public static CrossTracker withRedis(LocationConfig config,
+                                         String host, int port) {
+        return new CrossTracker(config, new BigLocalDataStorage(host, port));
+    }
+
+    /**
+     * @return {@link DataPreprocessor} to prepare raw data for analysis
+     */
     public DataPreprocessor getPreprocessor() {
         return preprocessor;
     }
 
+    /**
+     * @return {@link DataMiner} to manipulate and lookup data
+     */
     public DataMiner getMiner() {
         return miner;
     }
