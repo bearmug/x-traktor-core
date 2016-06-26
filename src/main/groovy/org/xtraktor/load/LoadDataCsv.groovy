363009b7643e4148bb28c096cb58dc1314f6a738
bearmug <pavel.fadeev@gmail.com>
2016-06-27 00:43:19 +0300

    cover csv loader with tests

--- src/main/groovy/org/xtraktor/load/LoadDataCsv.groovy
+++ src/main/groovy/org/xtraktor/load/LoadDataCsv.groovy
@@ -2,22 +2,31 @@ package org.xtraktor.load
 
 import groovy.transform.Canonical
 import org.xtraktor.DataPreprocessor
-import org.xtraktor.HashPoint
+import org.xtraktor.RawPoint
 
 import java.nio.file.Paths
-import java.util.stream.Stream
 
 @Canonical
 class LoadDataCsv implements LoadData {
 
     public static final String SEPARATOR = ','
-    private final String fileName
+    String fileName
 
     @Override
     void load(DataPreprocessor proc, int precision) {
+
         Paths.get(fileName).eachLine { line ->
-            def (hash, lon, lat, time, it) = line.trim().split(SEPARATOR)
-            new HashPoint(geoHashFull: hash, longitude: lon, latitude: lat, timestamp: time, userId: id)
+
+            if (line.trim().empty) {
+                return true
+            }
+
+            def (lon, lat, time, id) = line.trim().split(SEPARATOR)
+            proc.normalize([new RawPoint(
+                    longitude: lon as Double,
+                    latitude: lat as Double,
+                    timestamp: time as Long,
+                    userId: id as Long)])
         }
     }
 }
---
+++ src/test-commit/groovy/org/xtraktor/load/LoadDataCsvTest.groovy
@@ -0,0 +1,45 @@
+package org.xtraktor.load
+
+import org.xtraktor.DataPreprocessor
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class LoadDataCsvTest extends Specification {
+
+    private static final String RES_LOCATION = 'src/test-commit/resources/csv/'
+
+    @Unroll
+    def "correct file #file parsed"() {
+        given:
+        LoadData loader = new LoadDataCsv(fileName: RES_LOCATION + file)
+        DataPreprocessor proc = Mock(DataPreprocessor)
+
+        when:
+        loader.load(proc, precision)
+
+        then:
+        2 * proc.normalize(_)
+
+        where:
+        file          | precision
+        'valid-1.csv' | 6
+        'valid-2.csv' | 6
+    }
+
+    def "file with invalid/un-parseable content not parsed"() {
+        given:
+        LoadData loader = new LoadDataCsv(fileName: RES_LOCATION + file)
+        DataPreprocessor proc = Mock(DataPreprocessor)
+
+        when:
+        loader.load(proc, precision)
+
+        then:
+        thrown NumberFormatException
+
+        where:
+        file          | precision
+        'invalid-1.csv' | 6
+        'invalid-2.csv' | 6
+    }
+}
